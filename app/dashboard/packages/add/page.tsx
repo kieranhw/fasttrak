1d6e317ad77bcb0c12f389abb40c7718c499fd60
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-09-27 23:11:40 +0100

    init: add package page

--- app/dashboard/packages/add/page.tsx
+++ app/dashboard/packages/add/page.tsx
@@ -5,9 +5,12 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
 export default function AddPackage() {
+
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-        <p>add package</p>
+    <div className="flex flex-col w-full justify-start gap-2 mx-auto p-4 max-w-[1500px]">
+        <div className="inline-flex justify-between">
+            <h1 className="text-foreground font-bold text-3xl my-auto">Add Package</h1>
+        </div>
     </div>
-  )
+)
 }
