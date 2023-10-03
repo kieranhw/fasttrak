8446ec21db0c78b6ae0c76ab77df8ff66a8597ed
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-10-03 10:29:31 +0100

    chore: file structure

---
+++ app/dashboard/depot/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+export default function DeliverySchedule() {
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+        <p>Depot</p>
+    </div>
+  )
+}
