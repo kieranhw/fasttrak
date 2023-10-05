7b008666a7cd7509bfa196dab249c988cea5dc0e
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-10-05 16:56:59 +0100

    feat: add tracking link to manage packages

--- app/dashboard/packages/manage/components/columns.tsx
+++ app/dashboard/packages/manage/components/columns.tsx
@@ -14,11 +14,38 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import Link from "next/link"
+import { useState } from "react"
+
 
 export const columns: ColumnDef<Package>[] = [
+
     {
         accessorKey: "tracking_id",
         header: "Tracking ID",
+        cell: ({ row }) => {
+            const trackingId = row.getValue("tracking_id")?.toString()
+
+            return (
+                <div className="flex flex-col w-fit">
+                    <p>{trackingId}</p>
+                    <Link target="_blank" href={`/track/${trackingId}`}>
+                        <p className="text-sm text-foreground/50 hover:text-blue-500 hover:underline w-fit">View Tracking</p>
+                    </Link>
+                </div>
+            )
+        }
     },
     {
         accessorKey: "weight",
@@ -56,22 +83,49 @@ export const columns: ColumnDef<Package>[] = [
         id: "actions",
         cell: ({ row }) => {
             const p = row.original
+            const [removeDialogOpen, setRemoveDialogOpen] = useState(false)
 
             return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Edit Information</DropdownMenuItem>
-                        <DropdownMenuItem>Remove Package</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Edit Information</DropdownMenuItem>
+                            <AlertDialog open={removeDialogOpen} onOpenChange={setRemoveDialogOpen}>
+                                <AlertDialogTrigger asChild>
+                                    <Button variant="ghost" className="relative h-8 font-normal flex cursor-default select-none items-center rounded-sm px-2 py-1 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">Remove Package</Button>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>Remove Package</AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            This action cannot be undone. This will remove the package from
+                                            the system and cancel any scheduled deliveries.
+                                        </AlertDialogDescription>
+                                        <AlertDialogDescription className="text-foreground/50 hover:underline hover:text-blue-500 hover:cursor-pointer w-fit">
+                                            Made a mistake? Amend the package instead.
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                        <AlertDialogAction>Continue</AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    {/* Remove Package Alert */}
+
+                </>
+
             )
         },
     },
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        dropdownMenuItem: "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       },
       size: {
         default: "h-10 px-4 py-2",
