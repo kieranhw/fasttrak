4df119c8643fa46e5ff0b2a8a934adc5aa28b101
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-10-05 16:56:34 +0100

    feat: init new pages

--- app/dashboard/delivery/history/page.tsx
+++ app/dashboard/delivery/history/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 
-export default function Dashboard() {
+export default function DeliveryHistory() {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-        <p>dashboard</p>
+        <p>delivery history</p>
     </div>
   )
 }
--- app/dashboard/reports/page.tsx
+++ app/dashboard/reports/page.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-
-export default function Dashboard() {
+export default function Reports() {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-        <p>dashboard</p>
+        <p>reports</p>
     </div>
   )
 }
