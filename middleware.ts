a59b48ea4199056f38a1787744af2b429894242b
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-11-01 11:23:11 +0000

    fix: bug where session token becomes invalid or res 400

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,29 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { AuthApiError } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
-
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  let res = NextResponse.next()
 
   // Create a Supabase client configured to use cookies
   const supabase = createMiddlewareClient({ req, res })
 
-  // Refresh session if expired - required for Server Components
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  await supabase.auth.getSession()
+  try {
+    // Refresh session if expired - required for Server Components
+    await supabase.auth.getSession()
+  } catch (error) {
+    if ((error as AuthApiError).name === 'AuthApiError' && (error as AuthApiError).status === 400) {
+      console.error("Invalid Refresh Token:", (error as AuthApiError).message)
+      
+      // Log user out if refresh token invalid
+      res = NextResponse.redirect('/login')
+
+    } else {
+      // Handle other errors or re-throw them
+      console.error("Unhandled error in middleware:", error)
+    }
+  }
 
   return res
 }
