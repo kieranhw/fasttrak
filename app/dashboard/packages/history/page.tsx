8145d1f18a6179dad757ce03e79c58c56e03ad99
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-11-27 21:29:31 +0000

    feat: init package history

--- app/dashboard/packages/history/page.tsx
+++ app/dashboard/packages/history/page.tsx
@@ -1,10 +1,40 @@
 'use client'
 
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { useEffect, useState } from 'react';
+import { Package } from '@/types/package';
+import { supabase } from '@/pages/api/supabase-client';
+import { DataTable } from './components/data-table';
+import { columns } from './components/columns';
+import { db } from '@/lib/db/db';
 
-export default function History() {
+export default function ManagePackage() {
+  const [data, setData] = useState<Package[]>([]);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      // TODO: Fetch delivered packages only
+      const packages = await db.packages.fetch.all();
+      if (packages) {
+        setData(packages);
+        console.log(packages);
+      }
+    }
+    fetchData();
+  }, [reload]);
+
+  const refreshData = () => setReload(prev => !prev);
+
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-        <p>Package History</p>
+    <div className="flex flex-col w-full justify-start gap-4 mx-auto p-4 max-w-[1600px]">
+      <div className="inline-flex justify-between">
+        <h1 className="text-foreground font-bold text-3xl">History</h1>
+      </div>
+      <DataTable columns={columns(refreshData)} data={data} refreshData={refreshData} />
+
     </div>
   )
 }
