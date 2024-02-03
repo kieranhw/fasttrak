84d9496ca7e7c43c751b1df732fd1d5b8c0596a9
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-02-03 16:43:26 +0000

    update: change indicator from circle to rectangle

--- components/ui/stage-indicator.tsx
+++ components/ui/stage-indicator.tsx
@@ -1,10 +1,10 @@
 export const StageIndicators = ({ currentStage, totalStages }: { currentStage: number; totalStages: number }) => {
     return (
-        <div className="h-2 mt-4 flex justify-center gap-2">
+        <div className="h-4 mt-4 flex justify-center gap-2 items-center">
             {Array.from({ length: totalStages }).map((_, index) => (
                 <div
                     key={index}
-                    className={`border rounded-lg h-2 w-2 ${index + 1 === currentStage ? 'border-primary bg-primary' : 'bg-card'}`}
+                    className={`border h-1 w-8 ${index + 1 === currentStage ? 'border-primary bg-primary' : 'bg-border'}`}
                 ></div>
             ))}
         </div>
