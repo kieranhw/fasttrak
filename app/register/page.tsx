81d9d1cc7084638d0dc797fb8ade6e44be2344a1
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-02-05 12:21:34 +0000

    update: remove unused function

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -96,17 +96,6 @@ export default function Login({
         return true
     }
 
-    const signOut = async () => {
-        "use server";
-
-        const cookieStore = cookies();
-        const supabase = createClient(cookieStore);
-
-        await supabase.auth.signOut();
-
-        return redirect("/login");
-    }
-
     return (
         <div className="flex-1 flex flex-col w-full px-8 sm:max-w-[450px] justify-center gap-2">
             <div className="p-8 border rounded-lg drop-shadow-sm bg-gradient-to-b from-primary/25 via-card via-20% to-card">
