b22b90af833a38f71a386ed78d72a12facbab16b
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-02-08 15:43:33 +0000

    feat: add attributes to depot to save address lines in database

--- types/depot.ts
+++ types/depot.ts
@@ -3,6 +3,9 @@ import { UUID } from "crypto"
 export type Depot = {
     depot_id?: UUID
     store_id?: UUID
+    address_1?: string
+    address_2?: string
+    postcode?: string
     depot_name: string
     depot_lat: number
     depot_lng: number
