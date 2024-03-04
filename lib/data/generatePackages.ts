f9e2bdf886bc4d61249b697437ea54173716bafe
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-03-04 11:18:27 +0000

    update: priority types and package status

--- lib/data/generatePackages.ts
+++ lib/data/generatePackages.ts
@@ -1,4 +1,4 @@
-import { CurrentState, Package, PriorityType } from "@/types/package"
+import { CurrentState, Package, PackageStatus, PriorityType } from "@/types/package"
 import { Vehicle } from "@/types/vehicle"
 import { DeliverySchedule, DeliveryStatus } from "@/types/delivery-schedule"
 import { UUID } from "crypto"
@@ -15,7 +15,7 @@ export const generatePackages = async (numPackages: number): Promise<Package[]> 
     console.log("addresses:" + addresses.length);
 
     for (let i = 0; i < numPackages; i++) {
-        const priorities: PriorityType[] = ["Express", "Standard"];
+        const priorities: PriorityType[] = [PriorityType.Standard, PriorityType.Express];
         const randomNumber = faker.number.int({
             'min': 0,
             'max': priorities.length - 1
@@ -47,7 +47,7 @@ export const generatePackages = async (numPackages: number): Promise<Package[]> 
             sender_address_lat: senderAddress.lat,
             sender_address_lng: senderAddress.lng,
             sender_phone: "07" + faker.number.int({ min: 0o0, max: 999999999 }),
-            status: "Pending",
+            status: PackageStatus.Pending,
             current_state: CurrentState.Pending,
             delivery_attempts: 0,
             weight: faker.number.int({ min: 5, max: 20 }),
