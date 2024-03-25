21371cb6f5f8e2f2e0fd4bc5e51baa3faedc3f92
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-03-25 00:58:32 +0000

    update: styles of breadcrumb text

--- components/ui/breadcrumb-link.tsx
+++ components/ui/breadcrumb-link.tsx
@@ -9,13 +9,13 @@ interface BreadcrumbLinkProps {
 }
 
 export const BreadcrumbLink: React.FC<BreadcrumbLinkProps> = ({ href, text, lastItem }) => {
-    const textColor = lastItem ? "text-foreground" : "text-muted-foreground font-light";
-    const separator = !lastItem ? <div className="text-foreground/50 font-light"><GoChevronRight size={18} /></div> : null;
+    const textColor = lastItem ? "text-foreground text-md" : "text-md text-muted-foreground";
+    const separator = !lastItem ? <div className="text-foreground/50"><GoChevronRight size={18}/></div> : null;
 
     return (
         <>
             <div className="inline-flex gap-2 items-center">
-                {/* If it's the last item, we display it without a link */}
+                {/* If it's the last item, display it without a link */}
                 {lastItem ? (
                     <div className={textColor}>{text}</div>
                 ) : (
