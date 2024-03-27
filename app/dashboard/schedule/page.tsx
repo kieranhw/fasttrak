b1a0977c09912240f8f077487d3c47d09f1e8944
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-03-27 13:50:35 +0000

    feat: fetch store and depot for report

--- app/dashboard/schedule/page.tsx
+++ app/dashboard/schedule/page.tsx
@@ -7,10 +7,13 @@ import {
 } from "@/components/ui/tooltip"
 
 import { ScheduleDetails } from "./components/schedule-details";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ScheduleAnalysis } from "./components/schedule-analysis";
 import { useSearchParams } from "next/navigation";
 import { DeliverySchedule } from "@/types/delivery-schedule";
+import { Store } from "@/types/store";
+import { db } from "@/lib/db/db";
+import { Depot } from "@/types/depot";
 
 export default function ScheduleDeliveries() {
   // TODO: Fetch schedules here instead of in details
@@ -18,9 +21,29 @@ export default function ScheduleDeliveries() {
 
   const searchParams = useSearchParams();
   let mode = searchParams?.get('mode');
+  let date = searchParams?.get('date');
+
   if (!mode) mode = "details";
 
   const [deliverySchedules, setDeliverySchedules] = useState<DeliverySchedule[]>([]);
+  const [store, setStore] = useState<Store>();
+  const [depot, setDepot] = useState<Depot>();
+
+  useEffect(() => {
+    async function fetchStore() {
+      const res = await db.stores.fetch.forUser();
+
+      if (res.data) {
+        setStore(res.data);
+
+        const depot = await db.depots.fetch.forStore(res.data);
+        if (depot.data) {
+          setDepot(depot.data[0]);
+        }
+      }
+    }
+    fetchStore();
+  }, []);
 
   return (
     <TooltipProvider delayDuration={100}>
@@ -29,8 +52,8 @@ export default function ScheduleDeliveries() {
         {mode === "details" &&
           <ScheduleDetails mode={mode} schedules={deliverySchedules} setSchedules={setDeliverySchedules} />
         }
-        {mode === "analysis" && 
-          <ScheduleAnalysis mode={mode} schedules={deliverySchedules} />
+        {mode === "analysis" &&
+          <ScheduleAnalysis mode={mode} schedules={deliverySchedules} setSchedules={setDeliverySchedules} store={store} depot={depot} />
         }
       </div>
     </TooltipProvider >
