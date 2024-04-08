a0a48e4ab3467034478d20d851ef192cde440d2d
Kieran Hardwick <kieranhardwick260@gmail.com>
2024-04-08 20:47:17 +0100

    feat: add delivery time from schedule profile

--- lib/routing/algorithms/genetic-algorithm/crossover.ts
+++ lib/routing/algorithms/genetic-algorithm/crossover.ts
@@ -1,14 +1,21 @@
-import { VRPSolution, VehicleRoute } from "../../model/vrp";
+import { VRPSolution } from "@/lib/routing/model/VRPSolution";
+import { VehicleRoute } from "@/lib/routing/model/VehicleRoute";
 import { routeFitness } from "./fitness";
 
-export function crossover(clonedSolution: VRPSolution): VRPSolution {
+/**
+ * Performs crossover on the two fittest routes from the set of routes in the solution. Two random nodes are selected
+ * from each parent route and swapped to create two offspring.
+ * @param parentSolution The solution to perform crossover on
+ * @returns The offspring solution with the two fittest routes crossed over
+ */
+export function crossover(parentSolution: VRPSolution): VRPSolution {
     // Sort routes by fitness in descending order to get the two fittest routes
-    const sortedRoutes = clonedSolution.routes.sort((a, b) => routeFitness(b) - routeFitness(a));
+    const sortedRoutes = parentSolution.routes.sort((a, b) => routeFitness(b) - routeFitness(a));
 
     // Select the two fittest routes for crossover, ensuring there are enough routes
     if (sortedRoutes.length < 2) {
         console.error('Not enough routes to perform crossover.');
-        return clonedSolution; // Return the cloned solution unchanged if not enough routes
+        return parentSolution; // Return the cloned solution unchanged if not enough routes
     }
 
     const parent1 = sortedRoutes[0];
@@ -18,16 +25,22 @@ export function crossover(clonedSolution: VRPSolution): VRPSolution {
     const [offspring1, offspring2] = performCrossoverOnRoutes(parent1, parent2);
 
     // Replace the routes in clonedSolution with the offspring
-    clonedSolution.routes[sortedRoutes.indexOf(parent1)] = offspring1;
-    clonedSolution.routes[sortedRoutes.indexOf(parent2)] = offspring2;
+    parentSolution.routes[sortedRoutes.indexOf(parent1)] = offspring1;
+    parentSolution.routes[sortedRoutes.indexOf(parent2)] = offspring2;
 
-    return clonedSolution;
+    return parentSolution;
 }
 
-export function performCrossoverOnRoutes(parent1: VehicleRoute, parent2: VehicleRoute): [VehicleRoute, VehicleRoute] {
+/**
+ * Performs crossover on two vehicle routes. Two random segments are selected from each route and swapped to create two offspring.
+ * @param parent1 The first parent route
+ * @param parent2 The second parent route
+ * @returns An array containing the two offspring routes
+ */
+function performCrossoverOnRoutes(parent1: VehicleRoute, parent2: VehicleRoute): [VehicleRoute, VehicleRoute] {
     // Clone parents to avoid modifying originals
-    const clone1 = Object.assign(Object.create(Object.getPrototypeOf(parent1)), parent1);
-    const clone2 = Object.assign(Object.create(Object.getPrototypeOf(parent2)), parent2);
+    const clone1 = parent1.clone();
+    const clone2 = parent2.clone();
 
     const maxLen = Math.min(clone1.nodes.length-2, clone2.nodes.length-2);
 
@@ -45,7 +58,7 @@ export function performCrossoverOnRoutes(parent1: VehicleRoute, parent2: Vehicle
     clone2.nodes.splice(startIndex2, segmentSize, ...segment1);
 
     // Update measurements with a fixed delivery time of 5
-    const deliveryTime = 3; // TODO: Fixed delivery time, get from schedule profile
+    const deliveryTime = clone1.scheduleProfile.delivery_time;
     clone1.updateMeasurements(deliveryTime);
     clone2.updateMeasurements(deliveryTime);
 
